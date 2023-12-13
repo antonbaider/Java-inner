2ad34bc82f3f22dae49432ff631d931a1b36db19
antonbaider <email.b.anton@gmail.com>
2023-12-13 12:28:01 +0100

    done

--- src/main/java/jom/com/softserve/s3/task1/Pizza.java
+++ src/main/java/jom/com/softserve/s3/task1/Pizza.java
@@ -10,6 +10,27 @@ class Pizza {
 	private Pizza() {
 	}
 
+	public static PizzaBuilder base() {
+		return new PizzaBuilder();
+	}
+
+	public void setCheese(String cheese) {
+		this.cheese = cheese;
+	}
+
+	public void setMeat(String meat) {
+		this.meat = meat;
+	}
+
+	public void setSeafood(String seafood) {
+		this.seafood = seafood;
+	}
+
+	public void setVegetable(String vegetable) {
+		this.vegetable = vegetable;
+	}
+
+
 	public String getCheese() {
 		return cheese;
 	}
@@ -30,17 +51,54 @@ class Pizza {
 		return mushroom;
 	}
 
-//	public static PizzaBuilder base() {
-//		return new PizzaBuilder();
-//	}
+	public void setMushroom(String mushroom) {
+		this.mushroom = mushroom;
+	}
 
-	// Describe PizzaBuilder class here
+	public static class PizzaBuilder {
+		private Pizza pizza;
 
+		private PizzaBuilder() {
+			pizza = new Pizza();
+		}
+
+		public PizzaBuilder addCheese(String cheese) {
+			pizza.cheese = cheese;
+			return this;
+		}
+
+		public PizzaBuilder addMeat(String meat) {
+			pizza.meat = meat;
+			return this;
+		}
+
+		public PizzaBuilder addSeafood(String seafood) {
+			pizza.seafood = seafood;
+			return this;
+		}
+
+		public PizzaBuilder addVegetable(String vegetable) {
+			pizza.vegetable = vegetable;
+			return this;
+		}
+
+		public PizzaBuilder addMushroom(String mushroom) {
+			pizza.mushroom = mushroom;
+			return this;
+		}
+
+		public Pizza build() {
+			return pizza;
+		}
+	}
 }
 
 class Oven {
 	public static Pizza cook() {
-		// Create and return instance of Pizza class here
-		return null;
+		return Pizza.base()
+				.addCheese("Cheese")
+				.addMushroom("Mushroom")
+				.addSeafood("Seafood")
+				.build();
 	}
 }

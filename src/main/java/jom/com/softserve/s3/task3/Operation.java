2319bba0028dc026fe2920c32913d5e29bfe3692
antonbaider <email.b.anton@gmail.com>
2023-12-13 21:24:14 +0100

    done

--- src/main/java/jom/com/softserve/s3/task3/Operation.java
+++ src/main/java/jom/com/softserve/s3/task3/Operation.java
@@ -1,23 +1,46 @@
 package jom.com.softserve.s3.task3;
 
 public class Operation {
+	public static double execute(int a, int b, Strategy strategy) {
+		return strategy.doOperation(a, b);
+	}
 	public static double addAtoB(int a, int b) {
-		// Write your code here
-		return 0;
+		return execute(a, b, new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a + b;
+			}
+		});
 	}
 
 	public static double subtractBfromA(int a, int b) {
-		// Write your code here
-		return 0;
+		return execute(a, b, new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a - b;
+			}
+		});
 	}
 
 	public static double multiplyAbyB(int a, int b) {
-		// Write your code here
-		return 0;
+		return execute(a, b, new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				return a * b;
+			}
+		});
 	}
 
 	public static double divideAbyB(int a, int b) {
-		// Write your code here
-		return 0;
+		return execute(a, b, new Strategy() {
+			@Override
+			public double doOperation(int a, int b) {
+				if (b != 0) {
+					return a / b;
+				} else {
+					throw new IllegalArgumentException("Cannot divide by zero");
+				}
+			}
+		});
 	}
 }

cd105c2e18ea78bc1136231e0eccbaf02d50682f
antonbaider <email.b.anton@gmail.com>
2023-12-21 17:36:28 +0100

    refactoring

--- src/main/java/jom/com/softserve/s3/task4/Line.java
+++ src/main/java/jom/com/softserve/s3/task4/Line.java
@@ -4,12 +4,14 @@ package jom.com.softserve.s3.task4;
 import java.util.Locale;
 
 enum LineType {
-     SOLID, DOTTED, DASHED, DOUBLE;
+    SOLID, DOTTED, DASHED, DOUBLE;
 
- }
+}
+
 public class Line {
 
     public static String drawLine(LineType lineType) {
-        return "The line is " + lineType.name().toLowerCase(Locale.ENGLISH) + " type";
+        return String.format("The line is %s type", lineType.name().toLowerCase());
+
     }
 }
